621eedfd93d81b3a70947e37d97be2f8f2221cc8
Sonic0603 <78787756+Sonic0603@users.noreply.github.com>
2021-03-04 23:37:16 +0800

    Add files via upload

---
+++ s4api/proxy/lib/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	s4api: {
+		// username: process.env.S4_API_USERNAME,
+		// password: process.env.S4_API_PASSWORD,
+		username: "COM_API_USER",
+		password: "S4hanaIntegration12!",
+		domain: 'my300232-api.s4hana.ondemand.com'
+	}
+}
